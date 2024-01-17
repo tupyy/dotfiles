9ca4a95974f4e0020bbdad5ce862371266be884b
Cosmin Tupangiu <cosmin@redhat.com>
2024-01-17 21:40:32 +0100

    remove plugins

---
+++ conky/.config/conky/issues.lua
@@ -0,0 +1,106 @@
+#!/usr/bin/lua
+
+require 'luarocks.loader'
+local lunajson = require 'lunajson'
+local statusInProgress = "In Progress"
+local statusToDo = "To Do"
+local statusCodeReview = "Code Review"
+
+local function create_curl_req(token)
+  return "curl -s -G -H \"Accept: application/json\" -H \"Authorization: Bearer " ..
+      token ..
+      "\" " ..
+      "--data-urlencode \"jql=assignee=ctupangi\\u0040redhat.com and status!=6\" https://issues.redhat.com/rest/api/latest/search"
+end
+
+local function format(col_def, data)
+  local line = string.format("${goto %s}%s", col_def.key.offset, data.key)
+  -- status
+  line = string.format("%s${goto %s}%s", line, col_def.status.offset, data.status);
+  -- summary
+  line = string.format("%s${goto %s}%s", line, col_def.summary.offset, data.summary);
+  if data.status == statusInProgress then
+    return "${color yellow}" .. line .. "${color white}"
+  elseif data.status == statusCodeReview then
+    return "${color green}" .. line .. "${color white}"
+  end
+
+  return line
+end
+
+local function new_definition(x0)
+  local c = {}
+  c.key = {
+    header = "Issue";
+    offset = x0;
+  }
+  c.status = {
+    header = "Status";
+    offset = x0 + 150;
+  };
+  c.summary = {
+    key = "fields.summary";
+    header = "Summary";
+    offset = x0 + 300;
+  }
+  return c
+end
+
+local function exec(cmd)
+  local result = io.popen(cmd)
+  if (result == nil) then
+    return "", error("invalid result")
+  end
+  local output = result:read("*all")
+  result:close()
+  return output, nil
+end
+
+local function extract(data)
+  local extracted_data = {}
+  for _, issue in ipairs(data.issues) do
+    local d = {
+      key = issue.key,
+      status = issue.fields.status.name,
+      summary = issue.fields.summary
+    }
+    table.insert(extracted_data, d)
+  end
+  table.sort(extracted_data, function(a, b)
+    local aScore, bScore = 0, 0
+    if a.status == statusToDo then
+      aScore = 2
+    elseif a.status == statusInProgress then
+      aScore = 1
+    end
+    if b.status == statusToDo then
+      bScore = 2
+    elseif b.status == statusInProgress then
+      bScore = 1
+    end
+    return aScore > bScore
+  end)
+  return extracted_data
+end
+
+local function format_conky_table(col_definition, data)
+  local format_output = function(x, offset)
+    return string.format("${goto %s}", x + offset)
+  end
+
+  local table_string = "Issue" ..
+  string.format("${goto %s}Status", col_definition.status.offset) ..
+  string.format("${goto %s}Summary", col_definition.summary.offset)
+  for _, d in ipairs(data) do
+    local line = "";
+    line = format(col_definition, d)
+    table_string = string.format("%s\n%s", table_string, line)
+  end
+  return table_string
+end
+
+local pipelines_data = exec(create_curl_req("MzM4MTc5MTQyOTE3OiNPxnTY+ZhpzZ7nD3QVG535gjY8"))
+local data = extract(lunajson.decode(pipelines_data))
+
+local def = new_definition(550)
+print(format_conky_table(def, data))
---
+++ conky/.config/conky/pipelines.lua
@@ -0,0 +1,119 @@
+#!/usr/bin/lua
+
+require 'luarocks.loader'
+local lunajson = require 'lunajson'
+
+local pattern = "(%d+)%-(%d+)%-(%d+)T(%d+):(%d+):(%d+)%.(%d+)Z"
+
+local function create_curl_req(token)
+  return "curl -s -H \"PRIVATE-TOKEN: " ..
+      token .. "\" " .. "https://gitlab.cee.redhat.com/api/v4/projects/61167/pipelines"
+end
+
+local x = 550;
+local columns_def = {
+  [1] = {
+    key = "ref";
+    header = "Pipeline";
+    offset = 0;
+  };
+  [2] = {
+    key = "status";
+    header = "Status";
+    offset = 430;
+    transform = function(value)
+      local color = "${color white}"
+      if value == "success" then
+        color = "${color green}"
+      elseif value == "running" then
+        color = "${color yellow}"
+      elseif value == "failed" then
+        color = "${color red}"
+      end
+      return color .. value .. "${color white}"
+    end
+  };
+  [3] = {
+    key = "created_at";
+    header = "Created at";
+    offset = 560;
+    transform = function(value)
+      local xyear, xmonth, xday, xhour, xminute = value:match(pattern)
+      return xday .. "/" .. xmonth .. "/" .. xyear .. " " .. xhour .. ":" .. xminute
+    end
+  };
+  [4] = {
+    key = "updated_at";
+    header = "Updated at";
+    offset = 780;
+    transform = function(value)
+      local xyear, xmonth, xday, xhour, xminute = value:match(pattern)
+      return xday .. "/" .. xmonth .. "/" .. xyear .. " " .. xhour .. ":" .. xminute
+    end
+  };
+}
+
+local function exec(cmd)
+  local result = io.popen(cmd)
+  if (result == nil) then
+    return "", error("invalid result")
+  end
+  local output = result:read("*all")
+  result:close()
+  return output, nil
+end
+
+local function format_conky_table(initial_offset, columns, data)
+  local format_output = function(x, offset)
+    return string.format("${goto %s}", x + offset)
+  end
+
+  local header = ""
+  for _, c in ipairs(columns) do
+    header = string.format("%s%s%s", header, format_output(initial_offset, c.offset), c.header)
+  end
+
+  local table_string = string.format("%s", header)
+  for _, row in ipairs(data) do
+    local line = "";
+    for _, column in ipairs(columns_def) do
+      local value = row[column.key];
+      if (value ~= nil) then
+        if (column.transform ~= nil) then
+          value = column.transform(value)
+        end
+        line = string.format("%s%s%s", line, format_output(initial_offset, column.offset), value);
+      end
+    end
+    table_string = string.format("%s\n%s", table_string, line)
+  end
+  return table_string
+end
+
+local function format_console(data)
+  local header = ""
+
+  local table_string = ""
+  for _, row in ipairs(data) do
+    local line = string.format("%s", row);
+    table_string = string.format("%s\n%s", table_string, line)
+  end
+  return table_string
+end
+
+local runtime = arg[1] or "conky"
+local format
+if (runtime == "conky") then
+  format = format_conky_table
+else
+  format = format_console
+end
+
+local pipelines_data = exec(create_curl_req("9zmnQPAx-6MW9EeKy3bY"))
+local data = lunajson.decode(pipelines_data)
+
+if #data > 5 then
+  print(format(x, columns_def, { table.unpack(data, 1, 5) }))
+else
+  print(format(x, columns_def, data))
+end
